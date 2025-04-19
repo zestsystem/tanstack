fa3900fa7ed36dda1abafa92b1ed70617694bcee
zestsystem <mk337337@gmail.com>
2025-04-19 12:27:07 -0700

    feat: setup tailwindcss

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,48 +1,55 @@
 // app/routes/__root.tsx
-import type { ReactNode } from 'react'
+import type { ReactNode } from "react";
 import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
-} from '@tanstack/react-router'
+    Outlet,
+    createRootRoute,
+    HeadContent,
+    Scripts,
+} from "@tanstack/react-router";
+import appCss from "@/styles/app.css?url";
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-  }),
-  component: RootComponent,
-})
+    head: () => ({
+        meta: [
+            {
+                charSet: "utf-8",
+            },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            {
+                title: "TanStack Start Starter",
+            },
+        ],
+        links: [
+            {
+                rel: "stylesheet",
+                href: appCss,
+            },
+        ],
+    }),
+    component: RootComponent,
+});
 
 function RootComponent() {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
+    return (
+        <RootDocument>
+            <Outlet />
+        </RootDocument>
+    );
 }
 
 function RootDocument({ children }: Readonly<{ children: ReactNode }>) {
-  return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
+    return (
+        <html>
+            <head>
+                <HeadContent />
+            </head>
+            <body>
+                {children}
+                <Scripts />
+            </body>
+        </html>
+    );
 }
