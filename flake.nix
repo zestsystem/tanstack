3d5c82b00b9d46118cfa932db79e0298ebe89665
zestsystem <mk337337@gmail.com>
2025-04-19 12:25:01 -0700

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Tanstack Repro";
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              nodejs_20
+              pnpm
+            ];
+            shellHook = ''
+
+            '';
+          };
+        };
+      }
+    );
+}
